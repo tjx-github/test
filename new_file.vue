216db586115d90ba433009d4848ce4a1969f5e38
tjx-github <3040662002@qq.com>
2021-06-27 23:50:20 +0800

    asdfasf

--- new_file.vue
+++ new_file.vue
@@ -25,3 +25,19 @@ asfda
 分支操作
 )))))))))))))
 
+dfasdf
+dfasdfas
+dfas
+df
+a<transition
+	mode="out-in"
+	@before-enter="beforeEnter"
+	@enter="enter"
+
+	@before-leave="beforeLeave"
+	@leave="leave"
+	:css="false">
+
+</transition>fa<d v-for="item in items" :key="item.id">
+	{{ item }}
+</d>
